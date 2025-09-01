fbfb7bf9c0559bdc37b165e534d89f4171d6b8fc
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 09:47:24 +0300

    Created a class-based view that displays details for specific library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views.generic import ListView
 
 # Create your views here.
 def list_books (request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'relationship_app/list_libraries.html'
+    context_object_name = 'libraries'
