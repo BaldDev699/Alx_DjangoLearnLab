1796629d095a22e83d07aa6e01d1c937fcc53035
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 14:07:07 +0300

    Defined URL patterns to link to the authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.SignUpView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
