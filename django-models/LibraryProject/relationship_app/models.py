9619211104871c393e2fa51a1993fac6deaa2f52
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 08:31:09 +0300

    Created Library model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
-class Librarian(models.Model):
+class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
