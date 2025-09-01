2f51acae60e072457d362175f2cb182899c563be
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 09:07:22 +0300

    Query all books by a specific author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,7 @@
 from .models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.book_set.all()
-    except Author.DoesNotExist:
-        return Book.objects.none()
+    return Book.objects.filter(author__name=author_name)
 
 def get_books_in_library(library_name):
     try:
