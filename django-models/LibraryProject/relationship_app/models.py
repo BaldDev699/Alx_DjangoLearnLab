9b3e5ed9d5d501b99ff3802ef479845b0e184ed2
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 14:23:42 +0300

    Created a UserProfile model that includes a role field with predefined roles

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -27,3 +28,10 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=[('admin', 'Admin'), ('librarian', 'Librarian'), ('member', 'Member')])
+
+    def __str__(self):
+        return self.user.username
