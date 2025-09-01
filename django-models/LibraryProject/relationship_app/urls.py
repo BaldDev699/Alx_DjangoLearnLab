706339a25a84dd6a80fa8cdc89525b20a315d427
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-01 09:57:07 +0300

    Edit to include URL patterns that route to the newly created views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import list_books, LibraryDetailView
+
 from . import views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('libraries/', views.LibraryListView.as_view(), name='list_libraries'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
