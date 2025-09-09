c18d1266fea6892780fa9b0deac89cd762540c84
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-09 13:18:30 +0300

    0. Implementing a Custom User Model in Django

---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = ['can_add_book', 'can_change_book', 'can_delete_book']
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=[('admin', 'Admin'), ('librarian', 'Librarian'), ('member', 'Member')])
+
+    def __str__(self):
+        return self.user.username
---
+++ advanced_features_and_security/bookshelf/delete.md
@@ -0,0 +1,10 @@
+# Delete Operation - Book Model
+
+## Command
+from bookshelf.models import Book
+book = Book.objects.get(title="Nineteen Eighty-Four")
+book.delete()
+print(Book.objects.all())
+
+## Expected Output
+<QuerySet []>
