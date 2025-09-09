89798f4d718604119b939e010f05f5ee5e8ea509
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-09 13:33:23 +0300

    Create User Manager for Custom User Model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from LibraryProject.bookshelf.models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
