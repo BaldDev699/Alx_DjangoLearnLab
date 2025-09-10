c98bd9c63a7366a836bb8bd385b5d37005cc49c5
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-10 07:29:17 +0300

    Define Custom Permissions in Models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -26,3 +26,18 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self.create_user(username, email, password, **extra_fields)
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    class Meta:
+        permissions = [
+            ("can_view", "Can view article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
