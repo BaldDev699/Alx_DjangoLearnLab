f8c79fb217470bf75bf12524a221c3f888732d48
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-11 07:17:31 +0300

    Protect Views with CSRF Tokens

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'content']
+
+
+# ExampleForm demonstrates a basic Django form setup.
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea, required=False)
