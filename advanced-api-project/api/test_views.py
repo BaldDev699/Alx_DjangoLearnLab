11de80b8246c13c17196be603a591d1857533822
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-23 11:10:05 +0300

    Writing unit tests for Django REST Framework APIs

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,24 +1,28 @@
-from django.test import APITestCase
-from .models import Book
+from rest_framework.test import APITestCase
+from .models import Book, Author
 from rest_framework import status
 from django.contrib.auth.models import User
 
 class BookModelTest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.author = Author.objects.create(name="Test Author")
         self.book = Book.objects.create(
             title="Test Book",
-            author="Test Author",
-            published_year=2023
+            author=self.author,
+            publication_year=2023
         )
         self.list_url = '/api/books/list/'
         self.detail_url = f'/api/books/{self.book.id}/detail/'
     
     def test_book_creation(self):
+        # Login required for creating books
+        self.client.login(username='testuser', password='testpass')
+        new_author = Author.objects.create(name="New Author")
         data = {
             "title": "New Book",
-            "author": "New Author",
-            "published_year": 2024
+            "author": new_author.id,
+            "publication_year": 2024
         }
         response = self.client.post('/api/books/create/', data, format='json')
         self.assertEqual(response.status_code, 201)
@@ -36,19 +40,24 @@ class BookModelTest(APITestCase):
         self.assertEqual(response.data['title'], "Test Book")
 
     def test_book_update(self):
+        # Login required for updating books
         self.client.login(username='testuser', password='testpass')
-        response = self.client.get(self.detail_url, format='json')
+        updated_author = Author.objects.create(name="Updated Author")
         data = {
             "title": "Updated Book",
-            "author": "Updated Author",
-            "published_year": 2025
+            "author": updated_author.id,
+            "publication_year": 2025
         }
+        # Update view uses query parameter, not URL parameter
         response = self.client.put(f'/api/books/update/?id={self.book.id}', data, format='json')
         self.assertEqual(response.status_code, 200)
         self.book.refresh_from_db()
         self.assertEqual(self.book.title, "Updated Book")   
 
     def test_book_delete(self):
+        # Login required for deleting books
+        self.client.login(username='testuser', password='testpass')
+        # Delete view uses query parameter, not URL parameter
         response = self.client.delete(f'/api/books/delete/?id={self.book.id}', format='json')
         self.assertEqual(response.status_code, 204)
         self.assertEqual(Book.objects.count(), 0)
