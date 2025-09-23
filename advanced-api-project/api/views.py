bcde1f8308eecba7ec665f6c57c6176fc8205209
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-23 09:29:24 +0300

    Building custom views and generic views in django REST framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class ListView(generics.ListAPIView):
