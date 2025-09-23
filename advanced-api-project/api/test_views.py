08c68391c22f358d70f2703fe4652731011d3efd
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-23 10:40:10 +0300

    Writing unit tests for Django REST Framework APIs

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from .models import Book
+
+class BookModelTest(TestCase):
+    def setUp(self):
+        Book.objects.create(title="Test Book", author="Author A", published_year=2020)
+
+    def test_book_creation(self):
+        book = Book.objects.get(title="Test Book")
+        self.assertEqual(book.author, "Author A")
+        self.assertEqual(book.published_year, 2020)
+
+    def test_book_update(self):
+        book = Book.objects.get(title="Test Book")
+        book.author = "Author B"
+        book.save()
+        updated_book = Book.objects.get(title="Test Book")
+        self.assertEqual(updated_book.author, "Author B")
+
+    def test_book_deletion(self):
+        book = Book.objects.get(title="Test Book")
+        book.delete()
+        books = Book.objects.all()
+        self.assertEqual(len(books), 0)
