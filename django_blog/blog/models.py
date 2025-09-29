095487578f3de00350a622232a6e62a20843e0c6
BaldDev699 <methusellanyongesa004@gmail.com>
2025-09-29 14:09:27 +0300

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
@@ -27,7 +28,6 @@ class Comment(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    posts = models.ManyToManyField(Post, related_name='tags')
 
     def __str__(self):
         return self.name
