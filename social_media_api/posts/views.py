da67501cca1f1e0a896232837dc14bbd9be75803
BaldDev699 <methusellanyongesa004@gmail.com>
2025-10-07 09:41:14 +0300

    Create views to handle liking and unliking posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -60,80 +60,17 @@ class FeedView(generics.ListAPIView):
         following_users = user.following.all()
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
     
-from rest_framework import viewsets, permissions, filters, generics
-from rest_framework.pagination import PageNumberPagination
-from .models import Post, Comment, Like
-from notifications.models import Notification
-from .serializers import PostSerializer, CommentSerializer, LikeSerializer
-from django.contrib.contenttypes.models import ContentType
-from rest_framework import status, views, permissions
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission: only allow owners of an object to edit/delete it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 20
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class FeedPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 50
-
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    pagination_class = FeedPagination
-
-    def get_queryset(self):
-        user = self.request.user
-        following_users = user.following.all()
-        return Post.objects.filter(author__in=following_users).order_by('-created_at')
-    
 class LikePostView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         user = request.user
 
-        if Like.objects.filter(user=user, post=post).exists():
-            return Response({"detail": "You already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
+        like, created = Like.objects.get_or_create(user=user, post=post)
 
-        like = Like.objects.create(user=user, post=post)
+        if not created:
+            return Response({"detail": "You already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
 
         # Create a notification for the post author
         if post.author != user:
@@ -147,26 +84,12 @@ class LikePostView(views.APIView):
         return Response(LikeSerializer(like).data, status=status.HTTP_201_CREATED)
 
 
-class UnlikePostView(views.APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        user = request.user
-        like = Like.objects.filter(user=user, post=post).first()
-
-        if not like:
-            return Response({"detail": "You haven't liked this post."}, status=status.HTTP_400_BAD_REQUEST)
-
-        like.delete()
-        return Response({"detail": "Post unliked."}, status=status.HTTP_200_OK)
-
 
 class UnlikePostView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         user = request.user
         like = Like.objects.filter(user=user, post=post).first()
 
