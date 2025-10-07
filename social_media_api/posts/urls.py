85b1e33d4ef1c0e0100985c63164445a294665e3
BaldDev699 <methusellanyongesa004@gmail.com>
2025-10-07 08:36:01 +0300

    1. Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import PostViewSet, CommentViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
+
+urlpatterns = router.urls
