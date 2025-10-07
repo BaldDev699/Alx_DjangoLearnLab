daffb7c8fb434bbb59b12c9f5e56683dc10ba169
BaldDev699 <methusellanyongesa004@gmail.com>
2025-10-07 08:55:41 +0300

    Updated accounts/serializers.py to include followers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -41,3 +41,36 @@ class LoginSerializer(serializers.Serializer):
                 'token': token.key
             }
         raise serializers.ValidationError("Invalid Credentials")
+
+class ProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='following.count', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
+
+class FollowSerializer(serializers.Serializer):
+    target_user_id = serializers.IntegerField()
+
+    def validate_target_user_id(self, value):
+        try:
+            user = get_user_model().objects.get(id=value)
+        except get_user_model().DoesNotExist:
+            raise serializers.ValidationError("User does not exist")
+        return value
+
+    def save(self, **kwargs):
+        request_user = self.context['request'].user
+        target_user_id = self.validated_data['target_user_id']
+        target_user = get_user_model().objects.get(id=target_user_id)
+
+        if request_user == target_user:
+            raise serializers.ValidationError("You cannot follow/unfollow yourself")
+
+        if target_user in request_user.following.all():
+            request_user.following.remove(target_user)
+        else:
+            request_user.following.add(target_user)
+
+        return target_user
